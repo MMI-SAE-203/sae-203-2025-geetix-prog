9094fd66933e358dddc1ed4472c571bcb10fb2b8
geetix-prog <mathis.guellati@edu.univ-fcomte.fr>
2025-03-14 08:55:34 +0100

    backend change

--- backend/backend.mjs
+++ backend/backend.mjs
@@ -1,5 +1,5 @@
 import PocketBase from 'pocketbase' ;
-const pb = new PocketBase('http://127.0.0.1:8090') ;
+const pb = new PocketBase("https://miaullywood-sae.mathis-guellati.fr:443") ;
 
 export function getDayName(dateString) {
     const date = new Date(dateString);
